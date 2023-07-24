28d17d2787154c606a8cd594f8120899730a2a9b
Lucio Ruixo <54895404+LucioRuixo@users.noreply.github.com>
2023-07-23 21:10:41 -0300

    Versioning Tool implementation started

---
+++ Assets/Versioning Tool/Editor/VersioningToolWindow.cs
@@ -0,0 +1,22 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace LRCore
+{
+    public class VersioningToolWindow : EditorWindow
+    {
+        private const string Title = "Versioning Tool";
+        private const string Shortcut_Open = "#&v";
+
+        [MenuItem(LRCore.Signature + "/" + Title + " " + Shortcut_Open)]
+        public static void Open() => GetWindow<VersioningToolWindow>(Title);
+
+        private void OnGUI()
+        {
+            string version = "1.0.0";
+            // TODO: auto-detectar versión
+
+            GUILayout.Label($"Creating new version: {version}", EditorStyles.boldLabel);
+        }
+    }
+}

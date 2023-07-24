1b04d3e31d6a227c36f764f0facbcc1f8dbd0fab
Lucio Ruixo <54895404+LucioRuixo@users.noreply.github.com>
2023-07-23 21:10:21 -0300

    ScriptableWindow class added

---
+++ Assets/Package/Utils/Editor/ScriptableWindow.cs
@@ -0,0 +1,96 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace LRCore
+{
+    public class ScriptableWindow : EditorWindow
+    {
+        // TODO: estudiar esta clase
+
+        private ScriptableObject TargetObject { get; set; }
+        private Editor CustomEditor { get; set; }
+
+        private Vector2 scrollPos;
+
+        private List<ScriptableObject> TargetObjects { get; set; } = new List<ScriptableObject>();
+
+        public static void OpenWindow(ScriptableObject so)
+        {
+            // ScriptableWindow window = CreateInstance(typeof(ScriptableWindow)) as ScriptableWindow;
+            // if(window == null) return;
+            // window.SetTargetObject(so);
+            // window.ShowUtility();
+
+            ScriptableWindow window = (ScriptableWindow)GetWindow(typeof(ScriptableWindow), false);
+            if (window == null) return;
+            window.SetTargetObject(so);
+        }
+
+        private void SetTargetObject(ScriptableObject targetObject)
+        {
+            TargetObject = targetObject;
+            CustomEditor = Editor.CreateEditor(targetObject);
+            titleContent = new GUIContent(targetObject.name);
+
+            // Keep a record of the setting objects hierarchy
+            int newTargetObjIdx = TargetObjects.IndexOf(targetObject);
+            if (newTargetObjIdx >= 0)
+            {
+                // Showing an object from the hierarchy -> remove all elements in front of it
+                TargetObjects.RemoveRange(newTargetObjIdx + 1, TargetObjects.Count - (newTargetObjIdx + 1));
+            }
+            else
+            {
+                // Showing a new object -> Add it to the list
+                TargetObjects.Add(targetObject);
+            }
+        }
+
+        void OnGUI()
+        {
+            EditorGUIUtility.labelWidth = 200.0f;
+
+            if (CustomEditor)
+            {
+                EditorGUILayout.BeginHorizontal();
+
+                foreach (var targetObj in TargetObjects)
+                {
+                    if (GUILayout.Button(targetObj.name, GUILayout.Width(targetObj.name.Length * 9.0f)))
+                    {
+                        OpenWindow(targetObj);
+                        return;
+                    }
+                }
+
+                GUILayout.EndHorizontal();
+
+                EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+                scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+                CustomEditor.OnInspectorGUI();
+                EditorGUILayout.EndScrollView();
+            }
+        }
+    }
+
+    [CustomPropertyDrawer(typeof(ScriptableObject), true)]
+    public class CustomScriptableObjectDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return 0.0f;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUILayout.PropertyField(property, label, true);
+            if (GUILayout.Button("VER", GUILayout.Width(40))) ScriptableWindow.OpenWindow((ScriptableObject)property.objectReferenceValue);
+
+            GUILayout.EndHorizontal();
+        }
+    }
+}

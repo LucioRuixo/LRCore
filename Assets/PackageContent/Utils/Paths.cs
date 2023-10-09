29fb912356abd0f4720548b23b9670fb984b5e16
Lucio Ruixo <54895404+LucioRuixo@users.noreply.github.com>
2023-10-09 00:23:53 -0300

    Package folder renamed to PackageContent

--- Assets/PackageContent/Utils/Paths.cs
+++ Assets/PackageContent/Utils/Paths.cs
@@ -7,7 +7,7 @@ namespace LRCore.Utils
         public static readonly string assetsFolder = Application.dataPath;
         public static readonly string projectFolder = assetsFolder.Replace("/Assets", "");
         public static readonly string buildsFolder = $"{projectFolder}/Builds";
-        public static readonly string packageFolder = $"{assetsFolder}/Package";
+        public static readonly string packageFolder = $"{assetsFolder}/PackageContent";
         public static readonly string projectSettingsFolder = $"{projectFolder}/ProjectSettings";
 
         public static readonly string editorSettings = $"{projectSettingsFolder}/EditorSettings.asset";

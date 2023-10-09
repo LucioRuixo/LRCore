54528b7e101653a0d79e75ca1382e86b1506e2ec
Lucio Ruixo <54895404+LucioRuixo@users.noreply.github.com>
2023-10-09 08:51:17 -0300

    Packages folder path added to Paths

--- Assets/PackageContent/Utils/Paths.cs
+++ Assets/PackageContent/Utils/Paths.cs
@@ -8,6 +8,7 @@ namespace LRCore.Utils
         public static readonly string projectFolder = assetsFolder.Replace("/Assets", "");
         public static readonly string buildsFolder = $"{projectFolder}/Builds";
         public static readonly string packageFolder = $"{assetsFolder}/PackageContent";
+        public static readonly string packagesFolder = $"{projectFolder}/Packages";
         public static readonly string projectSettingsFolder = $"{projectFolder}/ProjectSettings";
 
         public static readonly string editorSettings = $"{projectSettingsFolder}/EditorSettings.asset";
